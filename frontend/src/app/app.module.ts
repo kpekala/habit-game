8904f9c1120430b0733efed691818528e7d3175a
Konrad Pękala <kgpekala@gmail.com>
2024-01-06 21:10:32 +0100

    add button component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent } from './utils/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
