d4a57c18a16da0077d496fd4ef5e5ff16aae5105
Konrad Pękala <kgpekala@gmail.com>
2024-01-09 01:06:33 +0100

    add simple menu

--- frontend/src/app/main/header/header.component.ts
+++ frontend/src/app/main/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  showingMenu = false;
 
+  changeMenuState() {
+    this.showingMenu = !this.showingMenu;
+  }
 }
