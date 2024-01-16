7b0b6f0fe33b837ae904b6d61ddd527b37cc933b
Konrad Pękala <kgpekala@gmail.com>
2024-01-16 21:55:09 +0100

    add tests for signing in

--- backend/src/main/java/com/kpekala/habitgame/domain/auth/dto/LoginRequest.java
+++ backend/src/main/java/com/kpekala/habitgame/domain/auth/dto/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.kpekala.habitgame.domain.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginRequest {
     private String emailAddress;
     private String password;
--- backend/src/test-integration/java/com/kpekala/habitgame/domain/auth/AuthServiceTest.java
+++ backend/src/test-integration/java/com/kpekala/habitgame/domain/auth/AuthServiceTest.java
@@ -1,5 +1,6 @@
 package com.kpekala.habitgame.domain.auth;
 
+import com.kpekala.habitgame.domain.auth.dto.LoginRequest;
 import com.kpekala.habitgame.domain.auth.dto.SignupRequest;
 import com.kpekala.habitgame.domain.auth.exception.UserExistsException;
 import com.kpekala.habitgame.domain.user.UserRepository;
@@ -7,6 +8,9 @@ import com.kpekala.habitgame.security.jwt.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -24,6 +28,7 @@ public class AuthServiceTest {
     JwtService jwtService;
 
     @Test
+    @DirtiesContext
     public void testSignUp_whenUserDoesNotExist_createsUser() {
         // Assume
         var request = new SignupRequest("Tester1", "test1@test.pl", "123456");
@@ -38,11 +43,34 @@ public class AuthServiceTest {
     }
 
     @Test
-    public void testCreateUser_whenUserExists_throwException() {
+    public void testSignUp_whenUserExists_throwException() {
         // Assume
-        SignupRequest signupRequest = new SignupRequest("Tester", "test@test.pl", "123456");
+        var request = new SignupRequest("Tester", "test@test.pl", "123456");
 
         // Act and assert
-        assertThrows(UserExistsException.class, () -> authService.signup(signupRequest));
+        assertThrows(UserExistsException.class, () -> authService.signup(request));
+    }
+
+    @Test
+    public void testSignIn_whenUserExists_signUserIn() {
+        // Assume
+        var request = new LoginRequest("test@test.pl", "123456");
+
+        // Act
+        var response = authService.signin(request.getEmailAddress(), request.getPassword());
+
+        // Assert
+        var user = userRepository.findByEmailAddress("test@test.pl").get();
+        assertTrue(SecurityContextHolder.getContext().getAuthentication().isAuthenticated());
+        assertTrue(jwtService.isTokenValid(response.getToken(), user));
+    }
+
+    @Test
+    public void testSignIn_whenUserDoesNotExist_throwException() {
+        // Assume
+        var request = new LoginRequest("test1@test.pl", "123456");
+
+        assertThrows(BadCredentialsException.class, () ->
+                authService.signin(request.getEmailAddress(), request.getPassword()));
     }
 }
