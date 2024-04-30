f219053a5fccb4807ca267cacd7bfee82b77d7f6
kpekala <kgpekala@gmail.com>
2024-04-30 16:45:25 +0200

    add db initialization script

---
+++ backend/db/init_db.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", password="admin", port="3305")
+
+cursor = mydb.cursor()
+
+cursor.execute("create database habits")
+cursor.execute("create user 'admin' identified by 'admin'")
+cursor.execute("grant all on habits.* to 'admin'")
--- backend/src/main/java/com/kpekala/habitgame/bootstrap/DataInitializer.java
+++ backend/src/main/java/com/kpekala/habitgame/bootstrap/DataInitializer.java
@@ -14,6 +14,7 @@ import com.kpekala.habitgame.domain.user.User;
 import com.kpekala.habitgame.domain.user.UserRepository;
 import com.kpekala.habitgame.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionStatus;
@@ -27,6 +28,7 @@ import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
@@ -43,12 +45,22 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        if(isDatabaseInitialized()) {
+            log.info("Database is already initialized");
+            return;
+        }
         setUpRoles();
         setUpUsers();
         setUpTasks();
         setUpHabits();
         setUpItems();
     }
+
+    private boolean isDatabaseInitialized() {
+        return roleRepository.findByName("user").isPresent();
+    }
+
+
 
     private void setUpRoles() {
         Role userRole = new Role("user");
