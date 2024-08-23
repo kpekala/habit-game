6d4ae293ec72a8f5a31ad4c6555d40b6939cd38c
kpekala <kgpekala@gmail.com>
2024-08-23 17:50:29 +0200

    fix cors mapping

--- backend/src/main/java/com/kpekala/habitgame/config/WebSecurityConfig.java
+++ backend/src/main/java/com/kpekala/habitgame/config/WebSecurityConfig.java
@@ -64,7 +64,6 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-        registry.addMapping("/**").allowedOrigins("http://localhost:3070");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200", "http://localhost:3070");
     }
 }
