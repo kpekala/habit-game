de32f7bf7c67fcd74786dfeb1f9fd49aae14e574
Konrad Pękala <kgpekala@gmail.com>
2024-09-02 16:27:14 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd backend
 ./gradlew build
 cd ../
-docker build -t habitgame/backend ./backend
-docker build -t habitgame/frontend ./frontend
+docker build -t kpekala/habitgame-backend ./backend
+docker build -t kpekala/habitgame-frontend ./frontend
