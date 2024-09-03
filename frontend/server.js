c206a1c28b8042d550a9ccadde4fd81491c919da
kpekala <kgpekala@gmail.com>
2024-09-03 16:40:42 +0200

    change server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const http = require("http");
 
 const app = express();
-const port = 3070;
+const port = 20205;
 
 app.use(express.static(__dirname + "/dist/frontend/"));
 
 app.get("/*", (req, res) => {
   res.sendFile(__dirname + "/dist/frontend/index.html");
+  console.log(__dirname + "/dist/frontend/index.html");
 });
 
 http.createServer(app).listen(port, () => {
