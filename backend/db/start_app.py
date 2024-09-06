5d420870849d2e11dac05edfc2156a75994244d5
kpekala <kgpekala@gmail.com>
2024-09-06 11:29:27 +0200

    update start_app

--- backend/db/start_app.py
+++ backend/db/start_app.py
@@ -2,11 +2,12 @@ import mysql.connector
 import subprocess
 
 def init_db():
-    mydb = mysql.connector.connect(host="db", user="root", password="admin", port="3306")
+    db = mysql.connector.connect(host="db", user="root", password="admin", port="3306")
 
-    cursor = mydb.cursor()
+    cursor = db.cursor()
 
     cursor.execute("create database habits")
+    db.close()
     # cursor.execute("create user 'admin' identified by 'admin'")
     # cursor.execute("grant all ON *.* TO 'admin'@'%';")
     # cursor.execute('flush privileges;')
@@ -15,12 +16,8 @@ def run_app():
     subprocess.run(["java", "-jar", "-Dspring.profiles.active=production", "/app.jar"])
 
 def main():
-    try:
-        init_db()
-    except:
-        print('Something went wrong with database initialization')
-    finally:
-        run_app()
+    init_db()
+    run_app()
 
 if __name__ == "__main__":
     main()
