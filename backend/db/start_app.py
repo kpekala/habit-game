6d8d857d8977ffc4e09c155e611fe6d36377b02b
kpekala <kgpekala@gmail.com>
2024-09-06 11:23:31 +0200

    only create db

--- backend/db/start_app.py
+++ backend/db/start_app.py
@@ -7,9 +7,9 @@ def init_db():
     cursor = mydb.cursor()
 
     cursor.execute("create database habits")
-    cursor.execute("create user 'admin' identified by 'admin'")
-    cursor.execute("grant all ON *.* TO 'admin'@'%';")
-    cursor.execute('flush privileges;')
+    # cursor.execute("create user 'admin' identified by 'admin'")
+    # cursor.execute("grant all ON *.* TO 'admin'@'%';")
+    # cursor.execute('flush privileges;')
 
 def run_app():
     subprocess.run(["java", "-jar", "-Dspring.profiles.active=production", "/app.jar"])
