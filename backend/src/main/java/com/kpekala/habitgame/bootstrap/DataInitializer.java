f113fca1a96bb5f575a7394ff11731bef69b1b06
Konrad Pękala <kgpekala@gmail.com>
2024-10-25 20:35:33 +0200

    improve add task card

--- backend/src/main/java/com/kpekala/habitgame/bootstrap/DataInitializer.java
+++ backend/src/main/java/com/kpekala/habitgame/bootstrap/DataInitializer.java
@@ -60,8 +60,6 @@ public class DataInitializer implements CommandLineRunner {
         return roleRepository.findByName("user").isPresent();
     }
 
-
-
     private void setUpRoles() {
         Role userRole = new Role("user");
         Role adminRole = new Role("admin");
